3751ccabab870f916159ae5c556281e072e71f42
aslon1213 <hamidovaslon1@gmail.com>
2023-04-14 20:43:21 +0500

    dev com

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import os
-import urllib.request
+import platform
 
 os.system("git clone https://github.com/aslon1213/Chemical-Equations")
 os.system("cd Chemical-Equations")
-os.system("python3 -m pip install -r Chemical-Equations/requirements.txt")
-os.system("python3 Chemical-Equations/manage.py runserver")
+# get system os type
+if platform.system() == "Windows":
+    os.system("python -m pip install pipenv")
+    os.system("python -m pipenv shell")
+elif platform.system() == "Linux":
+    os.system("python3 -m pip install pipenv")
+    os.system("python3 -m pipenv shell")
+elif platform.system() == "Darwin":
+    os.system("python3 -m pip install pipenv")
+    os.system("python3 -m pipenv shell")
+else:
+    print("Your system is not supported")
+    exit(0)
+os.system("python -m pip install -r Chemical-Equations/requirements.txt")
+os.system("python Chemical-Equations/manage.py runserver")
